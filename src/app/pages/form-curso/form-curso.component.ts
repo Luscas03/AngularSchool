b6ae6cc4193e0000719760b812df14c0a919210f
Luscas03 <58983721+Luscas03@users.noreply.github.com>
2020-08-04 11:38:28 -0300

    curso cadastrando no json server!

--- src/app/pages/form-curso/form-curso.component.ts
+++ src/app/pages/form-curso/form-curso.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../../models/curso';
 import { Usuario } from 'src/app/models/usuario';
+import { CursoService } from 'src/app/services/curso.service';
 @Component({
   selector: 'app-form-curso',
   templateUrl: './form-curso.component.html',
   styleUrls: ['./form-curso.component.css']
 })
 export class FormCursoComponent implements OnInit {
- curso:Curso = new Curso;
-  constructor() { }
+  curso: Curso = new Curso;
+  constructor(
+protected cursoService:CursoService
+
+  ) { }
 
   ngOnInit(): void {
     this.curso.ativo = true;
   }
-onsubmit(form){
-console.log("Curso:", this.curso, "Formulário:", form);
-if(form.invalid){
-  alert("Erro");
-}
-else {
-  alert("Cadastrado!")
-}
+  onsubmit(form) {
+    console.log("Curso:", this.curso, "Formulário:", form);
+    if (form.invalid) {
+      alert("Formulário Inválido!");
+    }
+    else {
+      this.cursoService.add(this.curso).subscribe(
+        res=>{
+          alert("Cadastrado!");
+          console.log(res);
+          
+        },
+        err=>{
+          alert("Erro ao cadastrar!");
+          console.error(err);
+          
+        }
+      )
+    
+    }
 
-  
+
+  }
 }
-}
- 
---
+++ src/app/services/curso.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment'
+import { Curso } from '../models/curso';
+@Injectable({
+  providedIn: 'root'
+})
+export class CursoService {
+  private conn: string = environment.localDB;
+  private colletion: string = "curso";
+  constructor(
+
+    private http: HttpClient,
+
+  ) { }
+  public add(curso: Curso) {
+    //http:3000 ~ LOCALHOST 
+  return this.http.post(this.conn + this.colletion, curso);
+  }
+}
